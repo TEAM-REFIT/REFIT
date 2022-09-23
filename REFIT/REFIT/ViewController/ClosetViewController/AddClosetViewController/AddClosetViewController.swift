ed071280506c94458e5789c084b2d07a3ac69f5c
dongyoon Kim <dongyoon.dev@gmail.com>
2022-09-23 23:19:44 +0900

    [2022.09.23] 1. made AddViewController initNavigationBar func

--- REFIT/REFIT/ViewController/ClosetViewController/AddClosetViewController/AddClosetViewController.swift
+++ REFIT/REFIT/ViewController/ClosetViewController/AddClosetViewController/AddClosetViewController.swift
@@ -4,6 +4,12 @@ class AddClosetViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        initNavigationBar()
+    }
+    
+    func initNavigationBar() {
+        //remove navigation bar item title
         self.navigationController?.navigationBar.topItem?.title = ""
     }
 }
