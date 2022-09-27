dd820c5c47950b547e0604794b13d050d3ff2f33
dongyoon Kim <dongyoon.dev@gmail.com>
2022-09-27 17:07:04 +0900

    [2022.09.27] 1. made AddClosetView camera func

--- REFIT/REFIT/ViewController/ClosetViewController/AddClosetViewController/AddClosetViewController.swift
+++ REFIT/REFIT/ViewController/ClosetViewController/AddClosetViewController/AddClosetViewController.swift
@@ -1,52 +1,58 @@
 import UIKit
 
-class AddClosetViewController: UIViewController,
-                               UIImagePickerControllerDelegate,
-                               UINavigationControllerDelegate {
+class AddClosetViewController: UIViewController {
     
     @IBOutlet weak var clothesImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         initNavigationBar()
         
-        // 카메라 기능 세팅
-        let camera = UIImagePickerController()
-        camera.sourceType = .camera
-        camera.allowsEditing = true
     }
     
     @IBAction func chageImgBtntapped(_ sender: Any) {
+        
+        let picker = UIImagePickerController()
+        picker.sourceType = .camera
+        picker.allowsEditing = true
+        picker.delegate = self
+        
         print("hi")
+        
         let actionSheet = UIAlertController(title: "의류 추가 방법", message: "의류 추가 방법을 선택해주세요.", preferredStyle: .actionSheet)
         actionSheet.addAction(UIAlertAction(title: "사진 촬영하여 추가하기", style: .default, handler: { _ in
-            print("사진 촬영하여 추가하기")
+                self.present(picker, animated: true)
         }))
         actionSheet.addAction(UIAlertAction(title: "앨범에서 추가하기", style: .default, handler: { _ in
             print("앨법에서 추가하기")
         }))
         actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
         self.present(actionSheet, animated: true)
     }
     
     func initNavigationBar() {
         //remove navigation bar item title
         self.navigationController?.navigationBar.topItem?.title = ""
+//        self.navigationItem.title = "옷 등록"
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont.pretendard(size: 18, family: .SemiBold)]
     }
-    
-    /// use photo
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        
-        if let image = info[UIImagePickerController.InfoKey(rawValue: "UIImagePickerControllerEditedImage")] as? UIImage {
-            clothesImageView.image = image
-        }
+}
 
+extension AddClosetViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         picker.dismiss(animated: true, completion: nil)
     }
     
-    /// cancel
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-            picker.dismiss(animated: true, completion: nil)
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        picker.dismiss(animated: true, completion: nil)
+        
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as?
+                UIImage else {
+            return
         }
+        clothesImageView.image = image
+    }
 }
