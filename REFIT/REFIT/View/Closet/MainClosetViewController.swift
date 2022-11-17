bbecc450d2bc257adaa4ca4f684b8dd4a7f32651
Drew <dongyoon.dev@gmail.com>
2022-11-17 22:53:18 +0900

    feat: 임시 로그아웃 버튼 제작

--- REFIT/REFIT/View/Closet/MainClosetViewController.swift
+++ REFIT/REFIT/View/Closet/MainClosetViewController.swift
@@ -50,17 +50,7 @@ class MainClosetViewController:UIViewController {
     }
     
     @IBAction func searchBtnTapped(_ sender: Any) {
-        let firebaseAuth = Auth.auth()
-        do {
-          try firebaseAuth.signOut()
-        } catch let signOutError as NSError {
-          print("Error signing out: %@", signOutError)
-        }
-        
-        let loginVC = getVC("LoginViewController")
-        loginVC.modalPresentationStyle = .fullScreen
-        loginVC.modalTransitionStyle = .crossDissolve
-        present(loginVC, animated: true)
+
     }
     
     
--- REFIT/REFIT/View/Setting/SettingViewController.swift
+++ REFIT/REFIT/View/Setting/SettingViewController.swift
@@ -5,4 +5,17 @@ class SettingViewController: UIViewController {
         super.viewDidLoad()
         
     }
+    @IBAction func logoutBtn(_ sender: Any) {
+        let firebaseAuth = Auth.auth()
+        do {
+          try firebaseAuth.signOut()
+        } catch let signOutError as NSError {
+          print("Error signing out: %@", signOutError)
+        }
+        
+        let loginVC = getVC("LoginViewController")
+        loginVC.modalPresentationStyle = .fullScreen
+        loginVC.modalTransitionStyle = .crossDissolve
+        present(loginVC, animated: true)
+    }
 }
