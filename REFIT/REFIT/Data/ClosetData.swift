27cf2cbbe9fd6a61ea2e8d0d535b1a0c32c4889d
Drew <dongyoon.dev@gmail.com>
2022-11-24 22:16:47 +0900

    feat: firebase 에서 ClosetData 받는 로직 구현

--- REFIT/REFIT/Data/ClosetData.swift
+++ REFIT/REFIT/Data/ClosetData.swift
@@ -11,18 +11,21 @@ import Foundation
 class ClosetData {
     static let shared = ClosetData()
     
+    //allCloset
     var allClosetData = [[String: Any]]()
+    var allClosetTitleData = [String]()
     
     private init() {}
 }
 
-func getClosetData() {
+func getAllClosetData() {
     FirebaseFirestoreManger.db.collection("Closet").whereField("userID", isEqualTo: FirebaseAuthManager.userID).getDocuments() { (querySnapshot, err) in
             if let err = err {
                 print("Error getting documents: \(err)")
             } else {
                 for document in querySnapshot!.documents {
                     ClosetData.shared.allClosetData.append(document.data())
+                    ClosetData.shared.allClosetTitleData.append(document.data()["title"] as! String)
                 }
             }
     }
