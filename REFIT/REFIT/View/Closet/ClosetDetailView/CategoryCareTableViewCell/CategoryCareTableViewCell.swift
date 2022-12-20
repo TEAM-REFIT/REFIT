c4d2c89530a01ff0c6d43a4c7f7684d5892d9fef
Drew <dongyoon.dev@gmail.com>
2022-12-20 10:06:43 +0900

    :art: Change CategoryCareTableViewCell name to ClothesTableViewCell

--- REFIT/REFIT/View/Closet/ClosetDetailView/CategoryCareTableViewCell/CategoryCareTableViewCell.swift
+++
@@ -1,8 +0,0 @@
-//
-//  CategoryCareTableViewCell.swift
-//  REFIT
-//
-//  Created by 김동윤 on 2022/12/19.
-//
-
-import Foundation
---
+++ REFIT/REFIT/View/Closet/ClosetDetailView/ClothesCareTableViewCell/ClothesCareTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  CategoryCareTableViewCell.swift
+//  REFIT
+//
+//  Created by 김동윤 on 2022/12/19.
+//
+
+import Foundation
+import UIKit
+
+class CategoryCareTableViewCell: UITableViewCell {
+    
+    @IBOutlet var title: UILabel!
+    @IBOutlet var Icon: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+    }
+}
