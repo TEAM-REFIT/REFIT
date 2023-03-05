dd79d27610e7b7ca77d94fd18a013dc6f9684636
Drew <dongyoon.dev@gmail.com>
2023-03-05 19:14:33 +0900

    :wrench: :: Remove collectionView compostionalLayout

--- REFIT/REFIT/View/Closet/ClosetDetailView/ClothesCareInfoModalView/ClothesCareInfoModalViewController.swift
+++ REFIT/REFIT/View/Closet/ClosetDetailView/ClothesCareInfoModalView/ClothesCareInfoModalViewController.swift
@@ -47,29 +47,29 @@ extension ClothesCareInfoModalViewController: UICollectionViewDataSource {
     
 }
 
-extension ClothesCareInfoModalViewController {
-    fileprivate func createCompostionalLayout() -> UICollectionViewLayout {
-        let layout = UICollectionViewCompositionalLayout {
-            (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
-            
-            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
-            
-            let item = NSCollectionLayoutItem(layoutSize: itemSize)
-            
-            item.contentInsets = NSDirectionalEdgeInsets(top: 100, leading: 100, bottom: 100, trailing: 100)
-            
-            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
-
-            let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitem: item, count: 1)
-            
-            // 그룹으로 섹션 만들기
-            let section = NSCollectionLayoutSection(group: group)
-            
-            // 섹션에 대한 간격 설정
-            section.contentInsets = NSDirectionalEdgeInsets(top: 100, leading: 100, bottom: 100, trailing: 100)
-            
-            return section
-        }
-        return layout
-    }
-}
+//extension ClothesCareInfoModalViewController {
+//    fileprivate func createCompostionalLayout() -> UICollectionViewLayout {
+//        let layout = UICollectionViewCompositionalLayout {
+//            (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+//
+//            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+//
+//            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+//
+//            item.contentInsets = NSDirectionalEdgeInsets(top: 100, leading: 100, bottom: 100, trailing: 100)
+//
+//            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+//
+//            let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitem: item, count: 1)
+//
+//            // 그룹으로 섹션 만들기
+//            let section = NSCollectionLayoutSection(group: group)
+//
+//            // 섹션에 대한 간격 설정
+//            section.contentInsets = NSDirectionalEdgeInsets(top: 100, leading: 100, bottom: 100, trailing: 100)
+//
+//            return section
+//        }
+//        return layout
+//    }
+//}
