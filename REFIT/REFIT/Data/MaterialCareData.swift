735165628832c8884305db6d8efc00fe5157e379
Drew <dongyoon.dev@gmail.com>
2023-03-13 09:18:59 +0900

    :wrench: :: Add MaterialCareData file

---
+++ REFIT/REFIT/Data/MaterialCareData.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  REFIT
+//
+//  Created by 김동윤 on 2023/03/09.
+//
+
+import Foundation
+
+struct MaterialCareData {
+    // cell setting
+    let cellMainText: [String]
+    let cellTip: [String]
+}
