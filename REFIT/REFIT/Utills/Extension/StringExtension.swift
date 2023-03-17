90c36f6d7b414ac63332178e84b7f126e043e4d6
Drew <dongyoon.dev@gmail.com>
2023-03-17 14:12:39 +0900

    :wrench: :: Add StringExtension

---
+++ REFIT/REFIT/Utills/Extension/StringExtension.swift
@@ -0,0 +1,23 @@
+//
+//  StringExtension.swift
+//  REFIT
+//
+//  Created by 김동윤 on 2023/03/17.
+//
+
+import Foundation
+
+extension String {
+    func substring(from: Int, to: Int) -> String {
+        guard from < count, to >= 0, to - from >= 0 else {
+            return ""
+        }
+        
+        // Index 값 획득
+        let startIndex = index(self.startIndex, offsetBy: from)
+        let endIndex = index(self.startIndex, offsetBy: to + 1) // '+1'이 있는 이유: endIndex는 문자열의 마지막 그 다음을 가리키기 때문
+        
+        // 파싱
+        return String(self[startIndex ..< endIndex])
+    }
+}
